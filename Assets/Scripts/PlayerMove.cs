a2c0c42d791be07811b79d010580d9f15b9e7288
Sesari97 <sesari97@gmail.com>
2022-12-20 13:32:27 +0100

    Player movement
    Added the movement and the jump of the player

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float runSpeed = 2;
+    public float jumpSpeed = 3;
+
+    Rigidbody2D rb2D;
+
+    
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKey("d"))
+        {
+            rb2D.velocity = new Vector2(runSpeed, rb2D.velocity.y);
+        }
+        
+        else if (Input.GetKey("a"))
+        {
+            rb2D.velocity = new Vector2(-runSpeed, rb2D.velocity.y);
+        }
+        else
+        {
+            rb2D.velocity = new Vector2(0, rb2D.velocity.y);
+        }
+        if (Input.GetKey("w") && CheckGround.isGrounded)
+        {
+            rb2D.velocity = new Vector2(rb2D.velocity.x, jumpSpeed);
+        }
+    }
+}
